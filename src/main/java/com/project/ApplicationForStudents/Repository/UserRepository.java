e6a34ba1205aca6ac9bedd0eea165e3fc249880f
Ihor Petryshyn <44348379+petryshyyn@users.noreply.github.com>
2021-05-29 20:35:04 +0300

    Delete src/main/java/com/project/ApplicationForStudents directory

--- src/main/java/com/project/ApplicationForStudents/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.ApplicationForStudents.Repository;
-
-
-import com.project.ApplicationForStudents.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String username);
-}
